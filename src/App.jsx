3b8b8fc40cc4045108df7ec3ff0e4dbf2b3fc754
Andras Berta <bertaandras10@gmail.com>
2026-08-24 23:37:16 +0200

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, {useState, useContext} from "react";
 
 import { Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import NavBar from "./components/NavBar";
 
 
 
@@ -9,7 +13,16 @@ function App() {
 
 
 
-  return ("");
+  return (
+    <div className="app">
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/auth/" element={<Auth/>}></Route>
+        <Route path="/checkout/" element={<Checkout/>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
